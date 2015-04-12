ea454e6c7a9c729f2e95f0bedf27fdcc6ae630fd
Rowan Knobel <rowan_knobel@hotmail.com>
2015-04-12 20:21:49 +0200

    modified:   AnnotationViewerPG3/build/classes/annotationviewerpg3/AnnotationViewerPG3.class 	modified:   AnnotationViewerPG3/nbproject/private/private.properties 	modified:   AnnotationViewerPG3/src/annotationviewerpg3/AnnotationViewerPG3.java 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateAnnotationGUI$1.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateAnnotationGUI$2.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateAnnotationGUI.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateAnnotationGUI.form 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$10.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$11.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$12.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$13.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$14.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$15.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$16.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$17.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$18.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$19.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$20.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$21.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$22.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$23.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$24.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$25.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$26.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$27.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$28.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$29.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$30.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/CreateGUI$9.class 	AnnotationViewerPG3/build/classes/annotationviewerpg3/Java2MySql.class

--- AnnotationViewerPG3/src/annotationviewerpg3/AnnotationViewerPG3.java
+++ AnnotationViewerPG3/src/annotationviewerpg3/AnnotationViewerPG3.java
@@ -1,12 +1,16 @@
 /*
  *Date of creation: 23-3-2015
- *Last update: 27-3-2015
+ *Last update: 12-4-2015
  *Authors: Rowan Knobel, Mike Aaldering, Thijn van Kempen
  *
  *This application is made for the purpose of viewing and creating annotations from and for a sequence, and save them in a database.
  *It will also allow the user to take said sequences and annotations out of the database to alter them.
  *
  *This application is not completed, and is at the moment only meant as a proof of concept.
+ *
+ *Currently known bugs: 
+ *When a part of a sequence is highlighted in a text area, and if one wants to highlight a sequence in a different area, you have to make sure you
+ *de-highlight the sequence in the first text area. Otherwise the selected highlight will be the former highlighted sequence.
  */
 package annotationviewerpg3;
 
